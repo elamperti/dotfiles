d5150d285bbe8ff21dc49c17a66ed73807db55da
Enrico Lamperti <mail@elamperti.com>
2016-06-21 22:49:25 -0300

    Add :crystal_ball::sparkles: prompt wizard

---
+++ common/prompt-wizard.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+
+prompt_wizard() {
+    local generator_args=""
+    local templates=()
+    local styles=()
+
+    pushd "$(dirname "${BASH_SOURCE[0]}")/../art/prompt/templates/" &> /dev/null
+
+    # To avoid filling a description for each template item, here's a unicode space v
+    templates=($(ls -d */|sed 's#/##'|awk '{a[$1]=$1}END{for(i in a)printf " "a[i]"   on "}'))
+
+    # Show dialog only if more than one template
+    if [ ${#templates[@]} -gt 3 ]; then
+        exec 3>&1;
+        local selected_template=$(dialog --keep-tite --title "Templates" \
+            --radiolist "Pick a Bash prompt template" 16 32 10\
+            ${templates[@]} \
+            2>&1 1>&3 \
+        )
+        exec 3>&-
+
+        # Throw error if none selected
+        [ -z "${selected_template}" ] && return 1
+    else
+        # Use the only template available
+        selected_template=$(ls -d */|sed 's#/##')
+    fi
+    generator_args="${generator_args} --template=${selected_template}"
+
+    # Get into the template folder
+    if [ -d ${selected_template}/styles/ ]; then
+        pushd "${selected_template}/styles/" &> /dev/null
+
+        # To avoid filling a description for each style item, I used a unicode space here v too
+        styles=($(ls *.json|sed 's#.json##'|awk '{a[$1]=$1}END{for(i in a)printf " "a[i]"   on "}'))
+
+        # Take a moment to appreciate how I nailed the unicode space position in those two comments
+
+        # Show dialog only if more than one style
+        if [ ${#styles[@]} -gt 3 ]; then
+            exec 3>&1;
+            selected_style=$(dialog --keep-tite --title "Template style" \
+                --radiolist "Pick a style from this template's options" 16 40 10\
+                ${styles[@]} \
+                2>&1 1>&3 \
+            )
+            exec 3>&-
+
+            # Throw error if none selected
+            [ -z "${selected_style}" ] && return 1
+        else
+            # Use the only style available
+            selected_template=$(ls *.json|sed 's#.json##')
+        fi
+        generator_args="${generator_args} --style=${selected_style}"
+
+        # Out of template's styles folder
+        popd &>/dev/null
+    else
+        log WARN "No style folder found for template ${selected_template}"
+    fi
+
+    dialog --keep-tite --title "Customization" --yesno "Do you want to change the displayed hostname?" 6 39
+    if [ $? -eq 0 ]; then
+        exec 3>&1;
+        local custom_hostname=$(dialog --keep-tite --title "Customize displayed hostname" \
+                                --inputbox "Enter a custom hostname:" 8 36 2>&1 1>&3)
+        exec 3>&-
+    fi
+
+    if [ -n "${custom_hostname}" ]; then
+        generator_args="${generator_args} --hostname=${custom_hostname}"
+    fi
+
+    # Get into the generator's folder
+    pushd ".." &>/dev/null
+
+    python generate.py ${generator_args} &>/dev/null
+    if [ $? -eq 0 ] && [ -f bash_prompt ]; then
+        # Already exists -- back it up!
+        if [ -f "$HOME/.bash_prompt" ]; then
+            mv "$HOME/.bash_prompt" "../../backups/$(date +%Y%m%d-%H%M%S)-bash_prompt"
+            log NOTICE "Backed up previous .bash_prompt"
+        fi
+        mv bash_prompt "$HOME/.bash_prompt"
+    fi
+
+    # Out of generator folder
+    popd &>/dev/null
+
+    # Out of templates folder
+    popd &>/dev/null
+}
--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@ source 'common/utils.sh'
 source 'common/packages.sh'
 source 'common/bundles.sh'
 source 'common/logger.sh'
+source 'common/prompt-wizard.sh'
 
 # Default config for bashlog
 LOG_FILE=`readlink -f "$(dirname $0)/setup.log"`
@@ -32,6 +33,9 @@ OPTIONS
         --no-updates
            Skip `apt-get update`. Not recommended unless you already updated.
 
+        -p, --prompt
+           Shows the prompt parser wizard
+
         -v, --verbose
            Makes setup more verbose, mostly useful for debugging.
 
@@ -58,6 +62,7 @@ filter_args() {
 
         case $arg in
             -b|--bundle)
+                NO_UPDATES=1;
                 if [ -n "$2" ]; then
                     if bundle_exists $2; then
                         bundle_count=1
@@ -90,7 +95,10 @@ filter_args() {
             --no-updates)
                 NO_UPDATES=1;
                 ;;
-
+            -p|--prompt)
+                NO_UPDATES=1;
+                JUST_PROMPT=1;
+                ;;
             -v|-vv|-vvv|--verbose)
                 STDOUT_LOG_LEVEL="DEBUG"
                 log NOTICE "Verbosity set to ${STDOUT_LOG_LEVEL}."
@@ -114,6 +122,22 @@ init_git_submodules() {
     fi
     git submodule update &>/dev/null
     # It won't check if update succeeds because setup may be run offline
+}
+
+pick_prompt() {
+    if cmd_exists "python"; then
+        log INFO "Changing Bash prompt"
+        pretty_print INDENT RIGHT 3
+        prompt_wizard
+        if [ $? -eq 0 ]; then
+            log OK "Done"
+        else
+            log FAIL "Failed"
+        fi
+        pretty_print INDENT LEFT 3
+    else
+        log WARN "Skipping custom Bash prompt creation"
+    fi
 }
 
 print_splash() {
@@ -169,6 +193,12 @@ main() {
 
     create_backup_directory && log INFO "Backup folder created"
 
+    if [[ -v JUST_PROMPT ]]; then
+        test_for "python" OR_ABORT
+        pick_prompt
+        exit
+    fi
+
     if [[ ! -v JUST_BUNDLES ]]; then
         # Create a tarball of previous backup so ./backups folder is (almost) empty
         local backup_postfix="dotfiles_backup.tar.gz"
@@ -193,7 +223,10 @@ main() {
 
         set_keyboard_layout
 
+        test_for "python" OR_WARN "Python is needed to generate a custom Bash prompt."
+        pick_prompt
         #pick_motd
+
         pick_packages
     fi
 
