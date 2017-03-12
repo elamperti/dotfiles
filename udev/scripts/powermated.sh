93530ba31bcc697fa7976077606322fe350035a9
Enrico Lamperti <mail@elamperti.com>
2017-03-12 19:28:06 -0300

    Add udev rule+script for Griffin Powermate :control_knobs:

---
+++ udev/scripts/powermated.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Kill previous instances of powermated
+prev_instances="$(pgrep -f "python .*powermated")"
+if [ -n "${prev_instances}" ]; then
+    # shellcheck disable=SC2086
+    kill ${prev_instances}
+fi
+
+# Determine most probable user to execute powermated
+prob_user=$(who|grep " :0"|head -n1|awk '{print $1}')
+
+# Start powermated
+if command -v /usr/local/bin/powermated >/dev/null; then
+    echo 'sleep 1 && powermated &'| su - "${prob_user:-elamperti}" -c 'at now' -s /bin/sh
+fi
