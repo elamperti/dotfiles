50af2bb5d85779fd273ffd73a12b4c5e0642bb05
Enrico Lamperti <mail@elamperti.com>
2017-03-12 19:26:37 -0300

    Add udev rule+script for C-Media audio device :speaker:

---
+++ udev/scripts/cmedia-audio.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Determine most probable user running the pulseaudio daemon
+prob_user="$(ps -p "$(pgrep pulseaudio)" --format user=)"
+
+# Set default output device
+if command -v pacmd >/dev/null; then
+    # shellcheck disable=SC2016
+    echo 'sleep 4 && pacmd set-default-sink "$(pacmd list-sinks|grep "name:.*C-Media"|sed "s/.*<\(.*\)>/\1/")" >/dev/null'| su - "${prob_user:-elamperti}" -c 'at now' -s /bin/sh
+fi
