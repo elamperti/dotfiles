e0acf6fbb1d2191bea06a8efe81affcab730d0fc
Enrico Lamperti <mail@elamperti.com>
2018-10-13 21:06:38 -0300

    Use bash for Spotify info script :boot:

---
+++ home/.config/i3/spotify_info.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+IFS=$'\n'
+CURRENT_SONG=($(dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata' | egrep -A2 "artist|title"|grep -oP '(?<=string \")(?!xesam).*(?=\")'))
+echo "${CURRENT_SONG[0]} - ${CURRENT_SONG[1]}"
