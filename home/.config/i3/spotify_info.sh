aa8cca4bcd5602889fbc1fedfda410ae357a3c52
Enrico Lamperti <mail@elamperti.com>
2018-10-23 23:30:52 -0300

    Remove superfluous data from song titles :scissors:

--- home/.config/i3/spotify_info.sh
+++ home/.config/i3/spotify_info.sh
@@ -2,4 +2,4 @@
 
 IFS=$'\n'
 CURRENT_SONG=($(dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata' | egrep -A2 "artist|title"|grep -oP '(?<=string \")(?!xesam).*(?=\")'))
-echo "${CURRENT_SONG[0]} - ${CURRENT_SONG[1]}"
+echo "${CURRENT_SONG[0]} - $(echo ${CURRENT_SONG[1]} | sed -E 's/ - (live (from|at|on).+|remastered.*)$//i')"
