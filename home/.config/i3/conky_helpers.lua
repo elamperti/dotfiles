ac0013268550769a30d66bcc2af89ec8c2a8d1e1
Enrico Lamperti <mail@elamperti.com>
2020-04-05 19:12:59 -0300

    Update i3 config as needed 🤹

--- home/.config/i3/conky_helpers.lua
+++ home/.config/i3/conky_helpers.lua
@@ -1,4 +1,6 @@
 -- Conky functions must be preceded by `conky_`
+
+-- Pads a string to the given length
 function conky_pad(padding, varname)
     value = conky_parse(varname)
     return value .. string.rep(" ", math.max(padding - #value, 0))
