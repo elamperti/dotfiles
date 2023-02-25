f6eb388663959edf2f185e6c51d5d648278c8502
Enrico Lamperti <elamperti@users.noreply.github.com>
2023-02-25 13:18:45 -0300

    Use kitty aliases only on kitty terminal 🐱

--- aliases/kitty.sh
+++ aliases/kitty.sh
@@ -3,10 +3,12 @@
 # Shortcuts related to kitty terminal
 # https://sw.kovidgoyal.net/kitty/
 
+if command -v "kitty" &>/dev/null; then
+  alias icat='kitty icat --align left'
+  alias isvg='rsvg-convert|icat'
+  alias kssh='kitty +kitten ssh'
 
-alias icat='kitty icat --align left'
-alias isvg='rsvg-convert|icat'
-alias kssh='kitty +kitten ssh'
-
-# Adds autocompletion for Bash
-source <(kitty + complete setup bash)
+  # Adds autocompletion for Bash
+  # Note: This was removed from kitty on v0.27 without warning
+  # source <(kitty + complete setup bash)
+fi
